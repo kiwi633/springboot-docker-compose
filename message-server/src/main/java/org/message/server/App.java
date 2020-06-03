4ed0de815fd588497c82e5a18f71fdb8b82bb753
17701253801@163.com <17701253801@163.com>
2020-06-03 19:46:41 +0800

    init code

---
+++ message-server/src/main/java/org/message/server/App.java
@@ -0,0 +1,18 @@
+package org.message.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+    @GetMapping("/get-message")
+    public String getName() {
+        return "say hello！";
+    }
+}
---
+++ policy-server/src/main/java/org/policy/server/App.java
@@ -0,0 +1,22 @@
+package org.policy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController("/")
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+    @GetMapping("/get-policy")
+    public String getName() {
+        return "policy";
+    }
+}
